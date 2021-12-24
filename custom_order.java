70ed0035fb5b7150e338a5b6b03e88aef04531c5
powerdreams-1 <95529902+powerdreams-1@users.noreply.github.com>
2021-12-24 17:46:01 +0300

    Update custom_order.java

--- custom_order.java
+++ custom_order.java
@@ -45,7 +45,17 @@ firstName = keyboard.nextLine();
 
 System.out.print(firstName + ", please see our MENU below: ");
 System.out.print("\n"); // skips a line
-      
+
+  System.out.println("_______________________________________________");      
+System.out.println("        MENU         QUANTITY    BASE COST  ");
+System.out.println("_______________________________________________"); 
+System.out.println("        Cake                     1            $15     ");
+System.out.println("   Set of Cupcakes       6            $15     ");
+System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+System.out.println("Frostings (vanilla, chocolate, strawberry, coco)");
+System.out.println("Fillings (mocha, mint, lemon, caramel, vanilla)");
+System.out.println("Toppings (sprinkles, cinnamon, cocoa, nuts)");
+System.out.println("_______________________________________________")    
 
 	  
 	  
